b474ba100b34bc8c437f2207802e5f26321bda80
KhahoryITLA <20164247@itla.edu.do>
2019-06-28 01:17:13 -0400

    Commit

---
+++ Presentacion5/Empleados.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentacion5
+{
+    class Empleados
+    {
+        //constructor
+        public Empleados()
+        {
+            Nombre = "";
+            edad = 0;
+            salario = 0.0m;
+        }
+
+        
+
+
+        private string _nombre;
+
+        //otra forma pro
+        public decimal salario { get; set; }
+
+        public int edad;
+        
+
+        public string Nombre { get => _nombre; set => _nombre = value; }
+
+        public decimal calcularSalario(int numDias)
+        {
+            return salario * numDias;
+        }
+
+    }
+}
---
+++ Presentacion5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentacion5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Empleados empleados = new Empleados();
+
+            //asignar valores
+            empleados.Nombre = "Angel";
+            empleados.edad = 20;
+            empleados.salario = 400000.00m;
+
+            
+
+            decimal total = empleados.calcularSalario(30);
+
+            //imprimir
+            Console.WriteLine("El nombre del empleado es: " +empleados.Nombre);
+            Console.WriteLine("La edad es: {0}", empleados.edad);
+            Console.WriteLine("Gana mnesual: " +total.ToString("C"));
+            Console.ReadKey();
+        }
+    }
+}
